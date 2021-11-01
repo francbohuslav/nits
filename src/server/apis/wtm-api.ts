7b411dc9cbda82fb10a701cf0569f249dc28e910
Bohuslav Franc <bohuslav.franc@unicornuniverse.eu>
2021-11-01 11:48:46 +0100

    No time sheets error validation - WTM sucks

--- src/server/apis/wtm-api.ts
+++ src/server/apis/wtm-api.ts
@@ -1,21 +1,40 @@
 import axios from "axios";
+import { assert } from "../../common/core";
 import { IMonthlyEvaluation, Timesheet } from "../models/uu/interfaces";
 
 export class WtmApi {
     private wtmUrl = "https://uuos9.plus4u.net/uu-specialistwtmg01-main/99923616732453117-8031926f783d4aaba733af73c1974840";
 
     public async listWorkerTimesheetItemsByMonth(idToken: string, year: number, month: number): Promise<Timesheet[]> {
-        const response = await this.serverRequest<IListWorkerTimesheetItemsByMonthResponse>(idToken, "listWorkerTimesheetItemsByMonth", {
-            yearMonth: year + (month + "").padStart(2, "0"),
-            pageInfo: {
-                pageIndex: 0,
-                pageSize: 10000,
-            },
-        });
-        if (response.pageInfo.total >= response.pageInfo.pageSize) {
-            throw new Error(`Příliš mnoho výkazů pro ${year}-${month}`);
+        assert(year);
+        assert(month);
+
+        try {
+            const response = await this.serverRequest<IListWorkerTimesheetItemsByMonthResponse>(idToken, "listWorkerTimesheetItemsByMonth", {
+                yearMonth: year + (month + "").padStart(2, "0"),
+                pageInfo: {
+                    pageIndex: 0,
+                    pageSize: 10000,
+                },
+            });
+            if (response.pageInfo.total >= response.pageInfo.pageSize) {
+                throw new Error(`Příliš mnoho výkazů pro ${year}-${month}`);
+            }
+            return this.convertToTimesheets(response.timesheetItemList);
+        } catch (ex) {
+            if (ex instanceof WtmError) {
+                if (
+                    ex.uuAppErrorMap &&
+                    Object.values(ex.uuAppErrorMap).length == 1 &&
+                    !ex.uuAppErrorMap["uu-specialistwtm-main/listWorkerTimesheetItemsByMonth/timesheetItemsNotFound"]
+                ) {
+                    throw ex;
+                }
+                return [];
+            } else {
+                throw ex;
+            }
         }
-        return this.convertToTimesheets(response.timesheetItemList);
     }
 
     public async listConfirmerMonthlyEvaluations(idToken: string, year: number, month: number): Promise<IMonthlyEvaluation[]> {
@@ -33,6 +52,7 @@ export class WtmApi {
     }
 
     public async listTimesheetItemsByMonthlyEvaluation(idToken: string, evaluationId: string): Promise<Timesheet[]> {
+        assert(evaluationId);
         const response = await this.serverRequest<IListWorkerTimesheetItemsByMonthResponse>(idToken, "listTimesheetItemsByMonthlyEvaluation", {
             monthlyEvaluation: evaluationId,
             pageInfo: {
@@ -82,6 +102,7 @@ export class WtmApi {
     }
 
     private async serverRequest<T extends IBaseResponse>(idToken: string, command: string, dtoIn: any): Promise<T> {
+        console.log("/" + command);
         const response = await axios.post(this.wtmUrl + "/" + command, dtoIn, {
             headers: { Authorization: `Bearer ${idToken}` },
         });
